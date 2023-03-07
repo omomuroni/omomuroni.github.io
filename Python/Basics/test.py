771192919b6ebb5f3190d2f0e9ecd219e1b95f60
omomuroni <omron.358@gmail.com>
2023-03-08 08:45:23 +0900

    Deployed 1817c61 with MkDocs version: 1.3.0

--- Python/Basics/test.py
+++ Python/Basics/test.py
@@ -1,15 +1,4 @@
-def sample_decorator(f):
-    def deco_wrapper(*args, **kwargs):
-        """デコレータのDocstringだよ"""
-        print("デコレータだよ")
-        return f(*args, **kwargs)
-    return deco_wrapper
+number = 12.3456
 
-@sample_decorator
-def deco_function():
-    """デコってる関数のDocstringだよ"""
-    print("これがデコってる関数だ！")
-
-
-if __name__ == '__main__':
-    deco_function()
+print(f'{number:.2f}')
+print(f'{number:.3g}')
---
+++ Python/Network/test_ipv6addr.py
@@ -0,0 +1,2 @@
+for i in open("test.txt", 'r'):
+    print(i)
