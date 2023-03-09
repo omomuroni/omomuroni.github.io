8761ca7b581c60fd2f0303bed789b63dcb1be1b7
omomuroni <omron.358@gmail.com>
2023-03-09 18:11:38 +0900

    Deployed 1817c61f with MkDocs version: 1.3.0

--- Python/Basics/test.py
+++ Python/Basics/test.py
@@ -1,4 +1,17 @@
-number = 12.3456
+import multiprocessing
+import time
+import os
 
-print(f'{number:.2f}')
-print(f'{number:.3g}')
+cpu_count = os.cpu_count()
+print(cpu_count)
+
+def parafunc (param):
+    print ("param {0} is being processed".format(param))
+    time.sleep (1)
+    return param * param
+
+# if __name__ == '__main__':
+with multiprocessing.Pool() as pool:
+    results = pool.map(parafunc, range(21))
+
+print (results)
